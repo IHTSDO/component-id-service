135904c4c980d538c115181098e93250147b86b7
jaykamal <jayakamala.n@telliant.net>
2025-03-20 18:05:12 +0530

    CIS-62 SCTID Information URL fails if trailing slash is included

--- src/main/java/org/snomed/cis/config/WebConfig.java
+++ src/main/java/org/snomed/cis/config/WebConfig.java
@@ -2,6 +2,7 @@ package org.snomed.cis.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,4 +12,21 @@ public class WebConfig implements WebMvcConfigurer {
     public void configurePathMatch(PathMatchConfigurer configurer){
         configurer.setUseTrailingSlashMatch(true);
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/info/js/*.ttf", "/info/js/*.woff2", "/info/js/*.woff")
+                .addResourceLocations("classpath:/static/info/fonts/roboto/");
+
+        registry.addResourceHandler("/info/index.html/js/**")
+                .addResourceLocations("classpath:/static/info/js/");
+
+        registry.addResourceHandler("/info/index.html/css/**")
+                .addResourceLocations("classpath:/static/info/css/");
+
+        registry.addResourceHandler("/info/index.html/fonts/roboto/**")
+                .addResourceLocations("classpath:/static/info/fonts/roboto/");
+    }
+
 }
