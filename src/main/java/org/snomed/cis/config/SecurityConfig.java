3174c75eed03a9efcaed1285094cdc83d9fe72c7
Quyen Ly <quocquyenit@gmail.com>
2025-11-13 17:50:51 +0700

    PIP-908 Tweak security config after switching to new spring-boot version 3.5.7

--- src/main/java/org/snomed/cis/config/SecurityConfig.java
+++ src/main/java/org/snomed/cis/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
 
@@ -63,8 +64,8 @@ public class SecurityConfig {
                 )
                 .addFilterBefore(new TokenAuthenticationFilter(authenticationManager()),
                         AnonymousAuthenticationFilter.class)
-                .cors(cors -> cors.disable())
-                .csrf(csrf -> csrf.disable());
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable);
         return httpSecurity.build();
 
     }
